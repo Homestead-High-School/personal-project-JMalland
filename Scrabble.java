3d3e035891da2a44ee1ef40ac0e358b80ed8f414
Jacob Malland <62392847+JMalland@users.noreply.github.com>
2022-11-02 15:59:45 -0500

    Edited the crossword parser. (That's what I'm calling it)

--- Scrabble.java
+++ Scrabble.java
@@ -1,100 +1,83 @@
-import java.util.TreeMap;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.ArrayList;
-import java.awt.Point;
-import java.util.Collections;
-import java.util.Comparator;
-
-public class Scrabble {
-    private static final int[][] board = new int[][] {
-        {4, 0, 0, 1, 0, 0, 0, 4, 0, 0, 0, 1, 0, 0, 4},
-        {0, 3, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 3, 0},
-        {0, 0, 3, 0, 0, 0, 1, 0, 1, 0, 0, 0, 3, 0, 0},
-        {1, 0, 0, 3, 0, 0, 0, 1, 0, 0, 0, 3, 0, 0, 1},
-        {0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0},
-        {0, 2, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 2, 0},
-        {0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0},
-        {4, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 4},
-        {0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0},
-        {0, 2, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 2, 0},
-        {0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0},
-        {1, 0, 0, 3, 0, 0, 0, 1, 0, 0, 0, 3, 0, 0, 1},
-        {0, 0, 3, 0, 0, 0, 1, 0, 1, 0, 0, 0, 3, 0, 0},
-        {0, 3, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 3, 0},
-        {4, 0, 0, 1, 0, 0, 0, 4, 0, 0, 0, 1, 0, 0, 4}
-    };
-    private char[][] map = new char[15][15];
-    private TreeMap<Character, HashMap<Integer, HashMap<String, String>>> list = null;
-    private ArrayList<Point> coords;
-    private ArrayList<Character> chars;
-
-    public Scrabble(TreeMap<Character, HashMap<Integer, HashMap<String, String>>> list) {
-        this.list = list;
-        coords = new ArrayList<Point>();
-    }
-
-    public void placeLetter(char l, int r, int c) {
-        if (r > map.length || c > map[0].length) {
-            throw new IllegalArgumentException("Index Out Of Bounds For Map Placement");
+import java.util.*;
+import java.io.File;
+public class Main {
+    public static void main(String[] args) {
+        TreeMap<Character, TreeMap<Integer, HashMap<String, String>>> map = parseAndStore("Words.txt"); // Get the sorted TreeMap
+        int numWords = 0;
+        for (char c : map.keySet()) { // Sorted by Character
+            //System.out.println(c+": ");
+            //int total = 0;
+            TreeMap<Integer, HashMap<String, String>> tempMap = map.get(c);
+            for (int n : tempMap.keySet()) { // Sorted again by Integer
+                numWords += tempMap.get(n).size();
+                //total += tempMap.get(n).size();
+                //System.out.println("\t"+n+":");
+                //System.out.println("\t\tSize: "+tempMap.get(n).size()); // Stored as a HashSet of Strings
+            }
+            //System.out.println("\tTotal Size: "+total+" Words");
         }
-        chars.add(l); // Append character
-        coords.add(new Point(r, c)); // Append character coordinates
-    }
-
-    public boolean validWordPlacement() {
-        // Sort rows & cols by both row and col index, to compare words in same column and row.
-        // https://stackoverflow.com/questions/2784514/sort-arraylist-of-custom-objects-by-property
-        Collections.sort(coords, new Comparator<Point>() { // Sort By Rows
-            @Override
-            public int compare(Point o1, Point o2) {
-                if (o1.y != o2.y) {
-                    return(o1.y - o2.y);
+        System.out.println(numWords+" Words!");
+        Scrabble basic = new Scrabble(map);
+        String one = "HAPPY";
+        String two = "UNHAPPY";
+        String three = "HELP";
+        /* End Result:
+            U N H A P P Y
+                E
+                L
+            H A P P Y
+        */
+        for (int r=0; r<12; r++) {
+            for (int c=0; c<12; c++) {
+                if (c < one.length() && r == 4) {
+                    basic.placeLetter(one.charAt(c), r, c);
+                    System.out.print(one.charAt(c)+" ");
                 }
-                else { // 2,3 2,5 2,6 2,4
-                    return(o1.x - o2.x);
+                //else if (c == one.length() && r == 4) {
+                    //System.out.println("Happy: "+basic.validWordPlacement());
+                //}
+                else if (r < 4 && r > 1 && c == 2) {
+                    basic.placeLetter(three.charAt(r-1), r, c);
+                    System.out.print(three.charAt(r-1)+" ");
+                }
+                else if (c < two.length() && r == 1) {
+                    basic.placeLetter(two.charAt(c), r, c);
+                    System.out.print(two.charAt(c)+" ");
+                }
+                //else if (c == two.length() && r == 1) {
+                    //System.out.println("UnHappy: "+basic.validWordPlacement());
+                //}
+                else {
+                    basic.placeLetter(' ', r, c);
+                    System.out.print(". ");
                 }
             }
-        });
-        String current = "";
-        int row = -1;
-        for (int i=0; i<coords.size(); i++) { // Loop through each character, forming each possible word in the list
-            Point p = coords.get(i);
-            if (row == -1 || p.y != row) {
-                row = p.y; // Set the current row
-                System.out.println(current); // Display Word
-                if (!current.equals("")) {
-                    if (!list.get(current.charAt(0)).get(current.length()).containsKey(current)) {
-                        return(false); // Map doesn't contain the word created by the combination of letters.
-                    }
-                    current = "";
-                }
-            }
-            current += chars.get(i);
+            System.out.println();
         }
-        Collections.sort(coords, new Comparator<Point>() { // Sort By Rows
-            @Override
-            public int compare(Point o1, Point o2) {
-                if (o1.x != o2.x) {
-                    return(o1.x - o2.x);
-                }
-                else { // 2,3 2,5 2,6 2,4
-                    return(o1.y - o2.y);
-                }
-            }
-        });
+        System.out.println("Words: "+basic.validWordPlacement());
     }
 
-    // Return the scrabble board
-    public static int[][] getBoard() {
-        return(Scrabble.board.clone()); // Return a clone of the board, so it isn't potentially edited
-    }
-
-    // Return the scrabble board tile
-    public static int getVal(int r, int c) {
-        if (r > Scrabble.board.length || c > Scrabble.board.length) {
-            throw new IllegalArgumentException("Index Out Of Bounds For Scrabble Board");
+    public static TreeMap<Character, TreeMap<Integer, HashMap<String, String>>> parseAndStore(String file) {
+        TreeMap<Character, TreeMap<Integer, HashMap<String, String>>> map = new TreeMap<Character, TreeMap<Integer, HashMap<String, String>>>(); // Default TreeMap to be returned
+        try {
+            Scanner input = new Scanner(new File(file)); // Use Scanner to parse the file
+            while (input.hasNextLine()) { // Loop through each line in the file
+                String temp = input.nextLine().trim(); // Store String for easy access
+                if (temp.contains("#")) continue; // Skip if the word is commented.
+                String[] arr = temp.trim().split("\\s", 2);
+                String define = arr[1];
+                temp = arr[0];
+                if (!map.containsKey(temp.charAt(0))) { // Add an empty TreeMap if it doesn't exist
+                    map.put(temp.charAt(0), new TreeMap<Integer, HashMap<String, String>>()); // Add Integer TreeMap
+                }
+                if (!map.get(temp.charAt(0)).containsKey(temp.length())) { // Add an emptuy HashMap if it doesn't exist
+                    map.get(temp.charAt(0)).put(temp.length(), new HashMap<String, String>());
+                }
+                map.get(temp.charAt(0)).get(temp.length()).put(temp, define); // Add the String and its definition to the HashMap
+            }
+        } catch (Exception e) { // Throw an exception, if necessary
+            System.out.println("Error: "+e); // Print the stack trace.
         }
-        return(Scrabble.board[r][c]);
+        return(map);
     }
 }
