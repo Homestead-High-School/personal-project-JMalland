621d3f50cff18345c77ee31cba7ad22d73a4412a
HappyZombieSlayer <62392847+HappyZombieSlayer@users.noreply.github.com>
2022-11-02 20:35:28 -0500

    Improved code so tiles can be built more efficiently.

--- Board.java
+++ Board.java
@@ -33,48 +33,37 @@ class Board extends JFrame implements ActionListener {
         for (int r=0; r<rows; r++) {
             for (int c=0; c<cols; c++) {
                 JButton temp = new JButton();
-                switch(Scrabble.getVal(r%rows, c%cols)) {
-                    case(1):
-                        temp = new JButton("2x L") {
-                            public void paintComponent(Graphics g) {
-                                Color temp = new Color(0x4274FF);
-                                g.setColor(new Color(temp.getRed(), temp.getGreen(), temp.getBlue(), 75)); // 75% Opacity
-                                g.fillRect(0, 0, getSize().width, getSize().height);
-                                super.paintComponent(g);
-                            }
-                        };
-                        break;
-                    case(2):
-                        temp = new JButton("3x L") {
-                            public void paintComponent(Graphics g) {
-                                g.setColor(new Color(0x4274FF));
-                                g.fillRect(0, 0, getSize().width, getSize().height);
-                                super.paintComponent(g);
-                            }
-                        };
-                        break;
-                    case(3):
-                        temp = new JButton("2x W") {
-                            public void paintComponent(Graphics g) {
-                                Color temp = new Color(0xD7381C);
-                                g.setColor(new Color(temp.getRed(), temp.getGreen(), temp.getBlue(), 75)); // 75% Opacity
-                                g.fillRect(0, 0, getSize().width, getSize().height);
-                                super.paintComponent(g);
-                            }
-                        };
-                        break;
-                    case(4):
-                        temp = new JButton("3x W") {
-                            public void paintComponent(Graphics g) {
-                                g.setColor(new Color(0xD7381C));
-                                g.fillRect(0, 0, getSize().width, getSize().height);
-                                super.paintComponent(g);
-                            }
-                        };
-                        break;
+                final int tile = Scrabble.getVal(r%rows, c%cols);
+                String text = "";
+                if (tile == 1) {
+                    text = "2x L"; // 2 * Letter Value
                 }
+                else if (tile == 2) {
+                    text = "3x L"; // 3 * Letter Value
+                }
+                else if (tile == 3) {
+                    text = "2x W"; // 2 * Word Value
+                }
+                else if (tile == 4) {
+                    text = "3x W"; // 3 * Word Value
+                }
+                temp = new JButton(text) {
+                    public void paintComponent(Graphics g) {
+                        Color color = new Color(0xFFFFFF);
+                        if (tile == 1 || tile == 2) { // Letter Bonus
+                            color = new Color(0x4274FF); // Blue Tile
+                        }
+                        else if (tile == 3 || tile == 4) { // Word Bonus
+                            color = new Color(0xD7381C); // Red Tile
+                        }
+                        g.setColor(new Color(color.getRed(), color.getGreen(), color.getBlue(), tile == 1 || tile == 3 ? 25 : 100)); // 75% Opacity, if tile is 1 or 3
+                        g.fillRect(0, 0, getSize().width, getSize().height);
+                        super.paintComponent(g);
+                    }
+                };
                 temp.setContentAreaFilled(false);
                 // https://stackoverflow.com/questions/33954698/jbutton-change-default-borderhttps://stackoverflow.com/questions/33954698/jbutton-change-default-border
+                // Maybe I should make the borders appear curved?
                 temp.setBorder(BorderFactory.createLineBorder(Color.black, 1));
                 // Action Listener: https://stackoverflow.com/questions/22580243/get-position-of-the-button-on-gridlayout
                 final int rIndex = r; // Make final so accessable at any time
@@ -82,6 +71,8 @@ class Board extends JFrame implements ActionListener {
                 temp.addActionListener(new ActionListener() {
                     @Override
                     public void actionPerformed(ActionEvent e) {
+                        // Pass the resultant actions to the Scrabble Game? // Board.getComponent(n)
+                        // Or, have a method return a list of all tiles, and add actionListeners inside a seperate class or the Scrabble class?
                         System.out.println("rowIndex "+rIndex+" columnIndex "+cIndex);
                     }
         
@@ -94,6 +85,10 @@ class Board extends JFrame implements ActionListener {
         frame.setSize(1000, 1000);
         frame.setVisible(true);
     }
+
+    @Override
     public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        
     }
 }
