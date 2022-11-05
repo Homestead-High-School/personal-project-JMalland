0c363d6e4d0dc5578e78740c7a9c81571426677d
HappyZombieSlayer <62392847+HappyZombieSlayer@users.noreply.github.com>
2022-11-05 09:41:48 -0500

    Stuff

--- Main.java
+++ Main.java
@@ -79,13 +79,7 @@ public class Main {
                 tiles[r][c].addActionListener(new ActionListener() {
                     @Override
                     public void actionPerformed(ActionEvent e) {
-                        //System.out.println("Tile clicked @ ("+rIndex+", "+cIndex+").");
-                        //JButton b = tiles[rIndex][cIndex];
-                        //b.setLayout(new BorderLayout());
-                        // The code below provides an example of how to change the specific x and y coordinates of a JComponent
-                        //board.setBounds(board.getBounds().y+(rIndex*10), board.getBounds().x+(cIndex*10), board.getWidth(), board.getHeight());
-                        //System.out.println(b.getBounds().x+" "+b.getBounds().y);
-                        //b.setBounds(b.getBounds().x+1, b.getBounds().y+1, b.getWidth(), b.getHeight());
+                        System.out.println("Tile clicked @ ("+rIndex+", "+cIndex+").");
                     }
                 });
             }
