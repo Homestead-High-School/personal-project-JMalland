d340d3661636cb3e1ec0bd69c95a2792066ed5e9
HappyZombieSlayer <62392847+HappyZombieSlayer@users.noreply.github.com>
2022-11-08 17:36:27 -0600

    Added clarifying comments

--- PaddedPanel.java
+++ PaddedPanel.java
@@ -30,16 +30,17 @@ public class PaddedPanel extends JPanel {
     }
 
     public void addPadding(Component comp) {
-        GridBagConstraints c = new GridBagConstraints();
-        // https://stackoverflow.com/questions/4842261/margin-padding-in-gridbaglayout-java
-        int LR = layout == PaddedPanel.X_AXIS ? padding : 0;
-        int UD = layout == PaddedPanel.X_AXIS ? 0 : padding;
+        // Keeping Components Square: https://stackoverflow.com/questions/48623643/how-to-make-gridbaglayout-specific-size
+        // Sizing Components With GridBagLayout: https://stackoverflow.com/questions/4842261/margin-padding-in-gridbaglayout-java
+        GridBagConstraints c = new GridBagConstraints(); // Creates constraints for the component
+        int LR = layout == PaddedPanel.X_AXIS ? padding : 0; // Figures if the padding is horizontal
+        int UD = layout == PaddedPanel.X_AXIS ? 0 : padding; // Figures if the padding is vertical
         c.insets = new Insets(UD, LR, UD, LR); // Insets(T, L, B, R)
-        c.weightx = 1;
-        c.weighty = 1;
-        c.fill = GridBagConstraints.BOTH;
-        GridBagLayout g = (GridBagLayout)(getLayout());
-        g.setConstraints(comp, c);
-        add(comp);
+        c.weightx = 1; // Sets the weight for the X-Padding. Determines the ratio for H/W Padding
+        c.weighty = 1; // Sets the weight for the Y-Padding. Determines the ratio for W/H Padding
+        c.fill = GridBagConstraints.BOTH; // Ensures both vertical and horizontal resizing
+        GridBagLayout g = (GridBagLayout)(getLayout()); // Gets the LayoutManager
+        g.setConstraints(comp, c); // Sets the constraints for the component
+        add(comp); // Adds the component
     }
 }
