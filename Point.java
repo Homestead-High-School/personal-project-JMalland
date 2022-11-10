beddc680c13e4065d0cfa0fd97107e137a99170c
HappyZombieSlayer <62392847+HappyZombieSlayer@users.noreply.github.com>
2022-11-09 21:10:40 -0600

    Created a Point class to hold [row][col] info.

---
+++ Point.java
@@ -0,0 +1,16 @@
+public class Point {
+    int r, c;
+
+    public Point(int r, int c) {
+        this.r = r;
+        this.c = c;
+    }
+
+    public int getRow() {
+        return(r);
+    }
+
+    public int getCol() {
+        return(c);
+    }
+}
