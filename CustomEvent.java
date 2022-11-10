5d228921e6000f80626b524bee980646411a0f7e
HappyZombieSlayer <62392847+HappyZombieSlayer@users.noreply.github.com>
2022-11-09 21:12:03 -0600

    Event/Listener class allows game-specific Events.

---
+++ CustomEvent.java
@@ -0,0 +1,34 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
+public class CustomEvent extends ActionEvent {
+    private int r = 0;
+    private int c = 0;
+    private int index = 0;
+
+    public CustomEvent(Component c, int id, int index) {
+        super(c, id, "");
+        this.index = index;
+    }
+
+    public CustomEvent(Component c, int id, int row, int col) {
+        super(c, id, "");
+        this.r = row;
+        this.c = col;
+    }
+
+    // Returns the index of the component, assuming it is within a single array
+    public int getIndex() {
+        return(index);
+    }
+
+    // Returns the row index of the component, assuming it is within a 2d array
+    public int getRow() {
+        return(r);
+    }
+
+    // Returns the col index of the component, assuming it is within a 2d array
+    public int getCol() {
+        return(c);
+    }
+}
---
+++ CustomListener.java
@@ -0,0 +1,6 @@
+public class CustomListener {
+    public void actionPerformed(CustomEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+}
