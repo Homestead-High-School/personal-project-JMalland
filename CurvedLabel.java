4bbb9ceedf033624d0a36a519bcf875186210fa1
HappyZombie_Slayer <jacob.malland@gmail.com>
2022-11-15 12:21:33 -0600

    Fixed stupid sizing issues.

--- CurvedLabel.java
+++ CurvedLabel.java
@@ -54,11 +54,11 @@ public class CurvedLabel extends JLabel {
         // Draw the background, if necessary
         if (background != null) { // If background isn't null
             g.setColor(background); // Set the color to the background color
-            g.fillRoundRect(0, 0, getSize().width, getSize().height, (int)(radius * frame.getWidth()/1056.0), (int)(radius * frame.getHeight()/1056.0)); // Draw the background
+            g.fillRoundRect(0, 0, getSize().width, getSize().height, (int)(radius * frame.getWidth()/840.0), (int)(radius * frame.getHeight()/1036.0)); // Draw the background
         }
 
         // Draw Label Text: https://stackoverflow.com/questions/5378052/positioning-string-in-graphic-java
-        g.setFont(new Font(font.getName(), font.getStyle(), (int)(font.getSize()*frame.getWidth()/1056.0)));
+        g.setFont(new Font(font.getName(), font.getStyle(), (int)(font.getSize()*frame.getWidth()/1036.0)));
         g.setColor(color);
         Rectangle2D rect = g.getFontMetrics().getStringBounds(text, g);
         g.drawString(text, (int)(getSize().width/2 - rect.getWidth()/2), (int)(getSize().height/2 + rect.getHeight()/2 - rect.getHeight()/4));
